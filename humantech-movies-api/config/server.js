eb156a5631c116528881f1a1cc39991227a552f2
Juan Ascanio <jascaniojs@gmail.com>
2020-11-20 11:41:19 -0500

    Strapi Init

---
+++ humantech-movies-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9cc903b869f2aac0b59af25ef12fdf56'),
+    },
+  },
+});
---
+++ humantech-movies-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2718d1f4-e167-433f-bea7-471c2b28bfbc'
+};
